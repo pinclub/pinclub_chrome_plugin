d863fd92e1222248009a12332b0a772fd81846d5
git <git123!>
2017-06-08 22:46:26 +0800

    Pinclub modify

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-# Pin It!
+# Pinclub chrome Plugin!
 
-A *very* simple chrome extension for quickly pining images from the web to [Pinterest](http://pinterest.com).
+A *very* simple chrome extension for quickly getting images from the web to [Jiuyanlou](http://jiuyanlou.com).
 
 ## How it works
 
-Just [install](https://github.com/downloads/kastner/pin_it/pinterest.crx) the extension in chrome, right click on any images you like, and select "Pin it!". Fill out the form, and boom!
-
+Just install the extension in chrome, right click on any images you like, and select "Get this Pictures!". Fill out the form, and boom!
 
 ## Note
 
-You have to be signed in to Pinterest for this to work ;).
-
-
-## Blog Post!
-
-I [blogged](http://metaatem.net/2011/12/05/quick-hack-pinterest-chrome-extension) about this little thing!
+You have to be signed in to Jiuyanlou.com for this to work ;).
--- content_script.js
+++ content_script.js
@@ -2,14 +2,12 @@ function refreshMenu() {
   chrome.contextMenus.removeAll();
 
   var imageMenu = chrome.contextMenus.create({
-    "title": "Pin It!", 
+    "title": "Get this Picture!", 
     "contexts": ["image"], "onclick": function (info, tab) {
-      console.log(info);
-      console.log(tab);
-      var pinUrl = "http://pinterest.com/pin/create/bookmarklet/";
-      pinUrl += "?media=" + escape(info.srcUrl) + "&url=" + escape(tab.url) + "&alt=alt&title=foo"; //+ escape(tab.title);
+      var pinUrl = "http://localhost:3000/image/create/bookmarklet/";
+      pinUrl += "?media=" + escape(info.srcUrl) + "&url=" + escape(tab.url) + "&title=" + escape(tab.title); //+ escape(tab.title);
       pinUrl += "&is_video=false&";
-      window.open(pinUrl,"pin"+(new Date).getTime(),"status=no,resizable=no,scrollbars=yes,personalbar=no,directories=no,location=no,toolbar=no,menubar=no,width=635,height=290,left=0,top=0");
+      window.open(pinUrl,"get"+(new Date).getTime(),"status=no,resizable=no,scrollbars=yes,personalbar=no,directories=no,location=no,toolbar=no,menubar=no,width=635,height=290,left=0,top=0");
     }
   });
 }
